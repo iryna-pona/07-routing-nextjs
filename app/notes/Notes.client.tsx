427c6d2d5da946af0b23f55b666b292678bd1211
iryna-pona <ipona3710@gmail.com>
2025-12-06 14:11:26 +0200

    Initial commit

---
+++ app/notes/Notes.client.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useDebounce } from 'use-debounce';
+import { fetchNotes } from '@/lib/api'; 
+import type { FetchNotesParams, FetchNotesResponse } from '@/lib/api';
+import NoteList from '@/components/NoteList/NoteList';
+import NoteForm from '@/components/NoteForm/NoteForm';
+import Modal from '@/components/Modal/Modal';
+import SearchBox from '@/components/SearchBox/SearchBox';
+import Pagination from '@/components/Pagination/Pagination';
+import ErrorMessage from '@/app/notes/error';
+import Loading from "@/app/loading";
+import css from './NotesPage.module.css';
+
+interface Props {
+  initialPage: number;
+  initialSearch: string;
+  perPage: number;
+}
+
+export default function NotesClient({ initialPage, initialSearch, perPage }: Props) {
+  const PER_PAGE = perPage ?? 12;
+
+  const [page, setPage] = useState(initialPage);
+  const [search, setSearch] = useState(initialSearch);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [debouncedSearch] = useDebounce(search, 500);
+
+  const handleSearch = (value: string) => {
+    setSearch(value);
+    setPage(1);
+  };
+
+  const params: FetchNotesParams = {
+    search: debouncedSearch,
+    page,
+    perPage: PER_PAGE,
+    sortBy: 'created',
+  };
+
+  const { data, isLoading, isError, error } = useQuery<FetchNotesResponse, Error>({
+    queryKey: ['notes', params.search, params.page],
+    queryFn: () => fetchNotes(params),
+    placeholderData: { notes: [], totalPages: 1 } as FetchNotesResponse,
+  });
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
+  if (isLoading) return <Loading />;
+  if (isError && error) return <ErrorMessage error={error} />;
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox value={search} onChange={handleSearch} />
+        {data?.totalPages && data.totalPages > 1 && (
+          <Pagination pageCount={data.totalPages} currentPage={page} onPageChange={setPage} />
+        )}
+        <button className={css.button} onClick={handleOpenModal}>
+          Create note +
+        </button>
+      </header>
+
+      <NoteList notes={data?.notes ?? []} />
+
+      {isModalOpen && (
+        <Modal onClose={handleCloseModal}>
+          <NoteForm onCancel={handleCloseModal} />
+        </Modal>
+      )}
+    </div>
+  );
+}
---
+++ components/NoteList/NoteList.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import type { Note } from '@/types/note';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteNote } from '@/lib/api';
+import css from './NoteList.module.css';
+
+interface NoteListProps {
+  notes: Note[];
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] }); // оновлюємо кеш
+    },
+  });
+
+  if (!notes.length) return <p>No notes found.</p>;
+
+  return (
+    <ul className={css.list}>
+      {notes.map(note => (
+        <li key={note.id} className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <Link href={`/notes/${note.id}`} className={css.view}>View details</Link>
+            <button className={css.button} onClick={() => mutation.mutate(note.id)}>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
